3c060eabe84732323d0a51ca803b404b555d8834
Hreaun <trollehunt@gmail.com>
2019-09-08 12:04:55 +0700

    done

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iomanip>
+#include <map>
+#include <set>
+#include "table.h"
+
+int main(int argc, char **argv) {
+  using namespace std;
+
+  table table;
+
+  ifstream in;
+  in.open(argv[1]);
+  ofstream out;
+  out.open(argv[2]);
+
+  string inStr;
+
+  while (getline(in, inStr)) {
+    table.currentStr = {};
+    for (char &c : inStr) {
+      if (isalpha(c) || (isdigit(c)))
+        table.currentStr += c;
+      else {
+        table.wordHandler();
+      }
+    }
+    table.wordHandler();
+  }
+
+  typedef function<bool(pair<string, int>, pair<string, int>)> Comparator;
+
+  Comparator compFunctor = [](const pair<string, int> &elem1,
+                              const pair<string, int> &elem2) {
+    return elem1.second >= elem2.second;
+  };
+
+  set<pair<string, int>, Comparator> setOfWords(table.word.begin(),
+                                                table.word.end(), compFunctor);
+
+  for (const pair<string, int> &element : setOfWords)
+    out << element.first << "," << element.second << "," << setprecision(3)
+        << element.second / table.wordCounter * 100 << "%" << endl;
+
+  in.close();
+  out.close();
+  return 0;
+}
---
+++ table.h
@@ -0,0 +1,34 @@
+#ifndef TASK0B_TABLE_H
+#define TASK0B_TABLE_H
+
+#include <map>
+#include <string>
+
+using namespace std;
+class table {
+public:
+  double wordCounter;
+  string currentStr;
+
+  table() {
+    wordCounter = 0;
+    currentStr = {};
+  }
+
+  map<string, int> word;
+
+  void wordHandler() {
+    if (!currentStr[0])
+      return;
+    table::wordCounter++;
+    if (!table::word.count(currentStr)) { // not found
+      table::word.insert(make_pair(currentStr, 1));
+      currentStr = {};
+    } else {
+      table::word[currentStr]++;
+      currentStr = {};
+    }
+  }
+};
+
+#endif // TASK0B_TABLE_H
