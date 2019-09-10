0451eb1959745b6e7802e6adbd7b06cb146d6be6
Hreaun <43428176+Hreaun@users.noreply.github.com>
2019-09-10 21:32:06 +0700

    Update table.h

--- lab0/table.h
+++ lab0/table.h
@@ -1,6 +1,9 @@
 #ifndef TASK0B_TABLE_H
 #define TASK0B_TABLE_H
 
+#include <fstream>
+#include <iomanip>
+#include <list>
 #include <map>
 #include <string>
 
@@ -10,6 +13,16 @@ private:
   double wordCounter;
   string currentStr;
   map<string, int> word;
+  list<pair<int, string>> listOfWords;
+
+  void mapSort() {
+    map<string, int>::iterator it;
+
+    for (it = word.begin(); it != word.end(); ++it)
+      listOfWords.emplace_back(make_pair(it->second, it->first));
+
+    listOfWords.sort(greater_equal<>());
+  }
 
 public:
   table() {
@@ -17,25 +30,28 @@ public:
     currentStr = {};
   }
 
-  double getWordCounter() { return wordCounter; }
-
-  auto getMapBegin() { return word.begin(); }
-
-  auto getMapEnd() { return word.end(); }
-
   void eraseStr() { currentStr = {}; }
 
   void addSimbol(char c) { currentStr += c; }
 
+
+  void print(ofstream &out) {
+    mapSort();
+
+    for (const pair<int, string> &element : listOfWords)
+      out << element.second << "," << element.first << "," << setprecision(3)
+          << element.first / wordCounter * 100 << "%" << endl;
+  }
+
   void wordHandler() {
     if (!currentStr[0])
       return;
-    table::wordCounter++;
-    if (!table::word.count(currentStr)) {
-      table::word.insert(make_pair(currentStr, 1));
+    wordCounter++;
+    if (!word.count(currentStr)) {
+      word.insert(make_pair(currentStr, 1));
       currentStr = {};
     } else {
-      table::word[currentStr]++;
+      word[currentStr]++;
       currentStr = {};
     }
   }
