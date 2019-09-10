f41596625c4b8c79f9ff967444a05f64c80062bd
Hreaun <43428176+Hreaun@users.noreply.github.com>
2019-09-10 21:31:36 +0700

    Update main.cpp

--- lab0/main.cpp
+++ lab0/main.cpp
@@ -1,7 +1,5 @@
 #include <fstream>
-#include <iomanip>
 #include <map>
-#include <set>
 #include "table.h"
 
 int main(int argc, char **argv) {
@@ -28,19 +26,7 @@ int main(int argc, char **argv) {
     table.wordHandler();
   }
 
-  typedef function<bool(pair<string, int>, pair<string, int>)> Comparator;
-
-  Comparator compFunctor = [](const pair<string, int> &elem1,
-                              const pair<string, int> &elem2) {
-    return elem1.second >= elem2.second;
-  };
-
-  set<pair<string, int>, Comparator> setOfWords(table.getMapBegin(),
-                                                table.getMapEnd(), compFunctor);
-
-  for (const pair<string, int> &element : setOfWords)
-    out << element.first << "," << element.second << "," << setprecision(3)
-        << element.second / table.getWordCounter() * 100 << "%" << endl;
+  table.print(out);
 
   in.close();
   out.close();
