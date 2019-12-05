b502946992a4e92ae61a6739b18d82bafd6b7557
Hreaun <43428176+Hreaun@users.noreply.github.com>
2019-12-05 15:31:03 +0700

    Update iBoard.h

--- iBoard.h
+++ iBoard.h
@@ -5,10 +5,14 @@
 #pragma once
 class iBoard {
  public:
-  virtual std::vector<std::string> getBoard() = 0;
+  virtual auto getBoard() -> std::vector<std::string> = 0;
   iBoard() = default;
   ~iBoard() = default;
-
 };
+
+void clearScreen() {
+  for (auto n = 0; n < 10; n++)
+    printf("\n\n\n\n\n\n\n\n\n\n");
+}
 
 #endif //THEGAME__IBOARD_H_
